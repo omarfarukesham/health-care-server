6a74177a9741a16ccb3550ba8e45deea6c5596b5
omarfarukesham <omar.lu86@gmail.com>
2025-04-25 09:38:42 +0600

    feat: image upload done

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -2,11 +2,18 @@ import  express  from "express";
 import { UserController } from "./user.controller";
 import auth from "../middlewares/auth";
 import { UserRole } from "@prisma/client";
+import { fileUloader } from "../../helpers/imageUploader";
+
 
 const router = express.Router();
 
+
 router.get("/",  auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),  UserController.getAllUsers);
-router.post("/",  UserController.createAdmin);
+router.post(
+    "/", 
+  
+    fileUloader.upload.single("file"),
+    UserController.createAdmin);
 
 
 export const UserRoutes = router;
